ea68631499a4b19676151e6049f8fbdf8fad7156
1440x900Fun <mdaniels029@protonmail.com>
2026-08-15 13:17:09 -0400

    Create server.py

---
+++ server/server.py
@@ -0,0 +1,16 @@
+# Dial7_server and switch (Central office)
+# audio freq bands
+# narrow 300-3.4khz
+# wide   50-7khz
+# superwide 50-14khz
+# full 20-20khz
+
+# needs bi-dir full dupe comms
+# progress tones
+# tone = (lowFhz,highFhz) | -1 N/A
+busy = (480,620)
+fastbusy = 0.25
+slowbusy = 0.50
+dial = (350,440)
+ring = (440,480)
+zip = (440,-1)
